6b32f9bde9445a0bddba70f6c89471029644111d
Md Rafi Uz Zaman <rafi.kazla@gmail.com>
2023-02-20 10:46:13 +0600

    final 12

--- script.js
+++ script.js
@@ -2,23 +2,28 @@ const currentTime = document.querySelector("h1"),
 content = document.querySelector(".content"),
 selectMenu = document.querySelectorAll("select"),
 setAlarmBtn = document.querySelector("button");
+
 let alarmTime, isAlarmSet,
 ringtone = new Audio("./files/ringtone.mp3");
+
 for (let i = 12; i > 0; i--) {
     i = i < 10 ? `0${i}` : i;
     let option = `<option value="${i}">${i}</option>`;
     selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
 }
+
 for (let i = 59; i >= 0; i--) {
     i = i < 10 ? `0${i}` : i;
     let option = `<option value="${i}">${i}</option>`;
     selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
 }
+
 for (let i = 2; i > 0; i--) {
     let ampm = i == 1 ? "AM" : "PM";
     let option = `<option value="${ampm}">${ampm}</option>`;
     selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
 }
+
 setInterval(() => {
     let date = new Date(),
     h = date.getHours(),
@@ -34,11 +39,13 @@ setInterval(() => {
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
     currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
+
     if (alarmTime === `${h}:${m} ${ampm}`) {
         ringtone.play();
         ringtone.loop = true;
     }
 });
+
 function setAlarm() {
     if (isAlarmSet) {
         alarmTime = "";
@@ -47,6 +54,7 @@ function setAlarm() {
         setAlarmBtn.innerText = "Set Alarm";
         return isAlarmSet = false;
     }
+
     let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
     if (time.includes("Hour") || time.includes("Minute") || time.includes("AM/PM")) {
         return alert("Please, select a valid time to set Alarm!");
@@ -56,5 +64,5 @@ function setAlarm() {
     content.classList.add("disable");
     setAlarmBtn.innerText = "Clear Alarm";
 }
-setAlarmBtn.addEventListener("click", setAlarm);
 
+setAlarmBtn.addEventListener("click", setAlarm);
